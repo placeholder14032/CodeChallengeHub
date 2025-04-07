67515be443bcc8f810298233ece694c8ba861c69
nazaninsmac <nzn.amabilis@gmail.com>
2025-04-07 06:56:25 +0330

    fixed a typo

--- web/routes/loginHandler.go
+++ web/routes/loginHandler.go
@@ -10,6 +10,11 @@ import (
 // @access public
 func  LoginUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Loginnnnnnn Userrrrrr")
+	// Parse the request body
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	fmt.Println("Username:", username)
+	fmt.Println("Password:", password)
 }
 
 // @desc login and check password for signin admin user
@@ -17,4 +22,15 @@ func  LoginUser(w http.ResponseWriter, r *http.Request) {
 // @access public
 func  LoginAdmin(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Loginnnnnnn Adminnnnn")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	fmt.Println("Username:", username)
+	fmt.Println("Password:", password)
+}
+
+
+func  checkUserStatus_Login(username, password string) bool {
+	// check id user exists in the database
+	// check if the password is correct
+	return true
 }
--- web/routes/signupHandler.go
+++ web/routes/signupHandler.go
@@ -10,6 +10,12 @@ import(
 // @access public
 func  SignupUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Signinnnnnnnn")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	passwordConfirm := r.FormValue("passwordConfirm")
+	fmt.Println("Username:", username)
+	fmt.Println("Password:", password)
+	fmt.Println("Confirm Password:", passwordConfirm)
 }
 
 
@@ -18,4 +24,18 @@ func  SignupUser(w http.ResponseWriter, r *http.Request) {
 // @access public
 func  SignupAdmin(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("Signinnnnnnnn")
+	username := r.FormValue("username")
+	corpEmail := r.FormValue("corp-email")
+	corpKey := r.FormValue("corp-key")
+	fmt.Println("Username:", username)
+
+	fmt.Println("Corp Email:", corpEmail)
+	fmt.Println("Corp Key:", corpKey)
+
+}
+
+func checkUserStatus_Signup(username, password string) bool {	
+	// check id user exists in the database
+	// check if the password is same as the confirm password
+	return true
 }
