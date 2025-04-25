26ceb1bb0c4215d0a8f7f9010416640dbaf4a3de
nazaninsmac <nzn.amabilis@gmail.com>
2025-04-25 14:41:07 +0330

    added error to signup page

--- web/routes/signupHandler.go
+++ web/routes/signupHandler.go
@@ -1,10 +1,8 @@
 package routes
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	// "os"
-	// "path/filepath"
 
 	"github.com/placeHolder143032/CodeChallengeHub/database"
 	"github.com/placeHolder143032/CodeChallengeHub/models"
@@ -15,60 +13,93 @@ import (
 // @desc creaing a new user account for signup
 // @route POST /api/auth/register-user
 // @access public
-func  SignupUser(w http.ResponseWriter, r *http.Request) {
-
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-	passwordConfirm := r.FormValue("passwordConfirm")
-
-	fmt.Print("Username:", username)
-	fmt.Print(", Password:", password)
-	fmt.Println(", Confirm Password:", passwordConfirm)
-
-	if (password==passwordConfirm){
-		// ok continue signing up
-
-		hashedPassword, err := HashPassword(password)
-		if err != nil {
-			http.Error(w, "Error hashing password", http.StatusInternalServerError)
-			return
-		}
-
-		// fmt.Println("Hashed Password:", hashedPassword)
-
-		// create user
-		createdUser := models.User{
-			Username: username,
-			Password: hashedPassword,
-		}
-
-		database.SignUpUser(createdUser)
-
-		// // Create user folder
-		// userFolder := filepath.Join("sources", username)
-		// err = os.MkdirAll(userFolder, 0755)
-		// if err != nil {
-		// 	http.Error(w, "Error creating user folder", http.StatusInternalServerError)
-		// 	return
-		// }
-		
-	// Redirect to profile page with status code 303 (See Other)
-	http.Redirect(w, r, "/profile", http.StatusSeeOther)
-	} else{
-		passwordMismatch()
-	}
-}
-
-func HashPassword(password string) (string, error) {
-    hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-    if err != nil {
-        return "", err
+func SignupUser(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
     }
-    return string(hash), nil
-}
 
-// TODO
-func passwordMismatch(){}
+    username := r.FormValue("username")
+    password := r.FormValue("password")
+    passwordConfirm := r.FormValue("passwordConfirm")
+
+    // Validation
+    if len(username) < 3 {
+        renderTemplate(w, "auth/userSignup.html", struct {
+            Error    string
+            Username string
+        }{
+            Error:    "Username must be at least 3 characters long",
+            Username: username,
+        })
+        return
+    }
+
+    if len(password) < 8 {
+        renderTemplate(w, "auth/userSignup.html", struct {
+            Error    string
+            Username string
+        }{
+            Error:    "Password must be at least 8 characters long",
+            Username: username,
+        })
+        return
+    }
+
+    if password != passwordConfirm {
+        renderTemplate(w, "auth/userSignup.html", struct {
+            Error    string
+            Username string
+        }{
+            Error:    "Passwords do not match",
+            Username: username,
+        })
+        return
+    }
+
+    // Hash password
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    if err != nil {
+        log.Printf("Error hashing password: %v", err)
+        renderTemplate(w, "auth/userSignup.html", struct {
+            Error    string
+            Username string
+        }{
+            Error:    "Internal server error",
+            Username: username,
+        })
+        return
+    }
+
+    user := models.User{
+        Username: username,
+        Password: string(hashedPassword),
+    }
+
+    // Save user to database
+    err = database.SignUpUser(user)
+    if err != nil {
+        var errorMsg string
+        if err.Error() == "user already exists" {
+            errorMsg = "Username already taken"
+        } else {
+            errorMsg = "Error creating account"
+            log.Printf("Error creating user: %v", err)
+        }
+
+        renderTemplate(w, "auth/userSignup.html", struct {
+            Error    string
+            Username string
+        }{
+            Error:    errorMsg,
+            Username: username,
+        })
+        return
+    }
+
+    // Redirect to login page on success
+    http.Redirect(w, r, "/login-user", http.StatusSeeOther)
+}
 
 
 
